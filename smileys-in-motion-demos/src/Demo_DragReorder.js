48c8ac125776ddf550d0943a3cf46af9c7be5496
Linton Ye <linton@jimulabs.com>
2020-10-29 09:27:47 -0700

    Add working code

--- smileys-in-motion-demos/src/Demo_DragReorder.js
+++ smileys-in-motion-demos/src/Demo_DragReorder.js
@@ -1,88 +1,87 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
+import {
+  animate,
+  motion,
+  useMotionValue,
+  useMotionTemplate,
+} from "framer-motion";
 import produce from "immer";
 import { useEffect } from "react";
 
-/*
+function Item({ title, index, onMove }) {
+  const y = useMotionValue(0);
+  const [isDragged, setDragged] = useState(false);
+  return (
+    <motion.li
+      style={{
+        cursor: "pointer",
+        background: "#444",
+        borderRadius: 8,
+        minWidth: 200,
+        marginTop: 8,
+        padding: 8,
+        position: "relative", // need this to enable z-index
+      }}
+      whileHover={{ scale: 1.1, zIndex: 1 }}
+      whileTap={{
+        scale: 1.2,
+        zIndex: 2,
+        boxShadow: "1px 1px 15px rgb(0,0,0,0.25)",
+      }}
+      drag="y"
+      layout
+      onDrag={(e, info) => {
+        const { top } = e.target.parentElement.getBoundingClientRect();
+        const targetIndex = Math.floor((info.point.y - top) / 83);
+        onMove(targetIndex);
+      }}
+      onDragStart={() => {
+        setDragged(true);
+      }}
+      onDragEnd={() => {
+        // TODO cancel the offset caused by drag
+        setDragged(false);
+      }}
+    >
+      {title}
+    </motion.li>
+  );
+}
 
- This example attempts to use animate to simulate drag&reorder. It's mostly working, however, there's an unwanted animation when dropping the dragged item. That seems difficult to resolve.
-
-*/
+function move(array, index, targetIndex) {
+  const x = array[index];
+  if (index > targetIndex)
+    for (let i = index; i > targetIndex; i--) {
+      array[i] = array[i - 1];
+    }
+  else
+    for (let i = index; i < targetIndex; i++) {
+      array[i] = array[i + 1];
+    }
+  array[targetIndex] = x;
+}
 
 export function Demo_DragReorder() {
-  const itemHeight = 83;
   const [faces, setFaces] = useState(
     "😇 😃 🥱 🤩 😢 😎 🤯".split(" ").map((f, idx) => `${idx} ${f}`)
   );
-  const [targetIndex, setTargetIndex] = useState(-1);
-  const [dragIndex, setDragIndex] = useState(-1);
   return (
     <ul style={{ listStyleType: "none", fontSize: 50 }}>
       {faces.map((f, index) => (
-        <motion.li
-          style={{
-            cursor: "pointer",
-            background: "#444",
-            borderRadius: 8,
-            minWidth: 200,
-            marginTop: 8,
-            padding: 8,
-            position: "relative", // need this to enable z-index
-          }}
-          whileHover={{ scale: 1.1, zIndex: 1 }}
-          whileTap={{
-            scale: 1.2,
-            zIndex: 2,
-            boxShadow: "1px 1px 15px rgb(0,0,0,0.25)",
-          }}
-          animate={{
-            y:
-              targetIndex === -1 || dragIndex === -1
-                ? 0
-                : dragIndex > targetIndex
-                ? targetIndex <= index && index < dragIndex
-                  ? itemHeight
-                  : 0
-                : dragIndex < index && index <= targetIndex
-                ? -itemHeight
-                : 0,
-          }}
+        <Item
+          index={index}
           key={f}
-          drag="y"
-          onDragStart={() => {
-            setDragIndex(index);
-          }}
-          onDrag={(e, info) => {
-            const targetIndex =
-              index +
-              (info.offset.y > 0
-                ? Math.ceil(info.offset.y / itemHeight - 0.4)
-                : Math.floor(info.offset.y / itemHeight + 0.4));
-            if (targetIndex !== index) setTargetIndex(targetIndex);
-          }}
-          onDragEnd={(e, info) => {
-            if (targetIndex >= 0) {
+          title={f}
+          onMove={(targetIndex) => {
+            if (0 <= targetIndex && targetIndex < faces.length) {
               setFaces(
                 produce(faces, (draft) => {
-                  const x = draft[index];
-                  if (index > targetIndex)
-                    for (let i = index; i > targetIndex; i--) {
-                      draft[i] = draft[i - 1];
-                    }
-                  else
-                    for (let i = index; i < targetIndex; i++) {
-                      draft[i] = draft[i + 1];
-                    }
-                  draft[targetIndex] = x;
+                  move(draft, index, targetIndex);
                 })
               );
             }
-            setDragIndex(-1);
-            setTargetIndex(-1);
           }}
-        >
-          {f}
-        </motion.li>
+        />
       ))}
     </ul>
   );
