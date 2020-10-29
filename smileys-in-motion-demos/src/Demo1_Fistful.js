69022f0754979974d5e6dea87c7babdd2b575216
Linton Ye <linton@jimulabs.com>
2020-10-29 15:52:39 -0700

    Add end result

--- smileys-in-motion-demos/src/Demo1_Fistful.js
+++ smileys-in-motion-demos/src/Demo1_Fistful.js
@@ -1,12 +1,16 @@
-import { motion } from "framer-motion";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 import * as React from "react";
 
 /* eslint-disable jsx-a11y/accessible-emoji */
 
 export function Demo1_Fistful() {
+  const scaleLeft = useMotionValue(1);
+  const scaleRight = useTransform(scaleLeft, (s) => 1 / s);
+  const rotateHead = useTransform(scaleLeft, [0.5, 2], [-10, 10]);
   return (
-    <div style={{ display: "flex", zIndex: 1 }}>
+    <div style={{ display: "flex" }}>
       <motion.div
+        style={{ scale: scaleLeft, zIndex: 1 }}
         animate={{
           scale: [1, 0.5, 2],
           transition: {
@@ -17,8 +21,8 @@ export function Demo1_Fistful() {
       >
         👊
       </motion.div>
-      <div>🤨</div>
-      <div style={{ transform: "scaleX(-1)" }}>👊</div>
+      <motion.div style={{ rotate: rotateHead }}>🤨</motion.div>
+      <motion.div style={{ scaleX: -1, scale: scaleRight }}>👊</motion.div>
     </div>
   );
 }
