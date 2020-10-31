16e306b86d0dd0285079a4c991b19b3e8d611665
Linton Ye <linton@jimulabs.com>
2020-10-30 17:03:20 -0700

    Turn off emoji eslint

--- smileys-in-motion-demos/src/Demo2_BodyParts.js
+++ smileys-in-motion-demos/src/Demo2_BodyParts.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { motion } from "framer-motion";
 
+/* eslint-disable jsx-a11y/accessible-emoji */
+
 export function Demo2_BodyParts() {
   return (
     <div style={{ display: "flex" }}>
