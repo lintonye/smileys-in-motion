8368de93f2bd9ae48dd6819e88a6a218974ae45a
Linton Ye <linton@jimulabs.com>
2020-11-06 23:13:19 -0800

    Assemble smiley

--- smileys-in-motion-demos/src/InitialStyleAnimate.js
+++ smileys-in-motion-demos/src/InitialStyleAnimate.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { motion } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
 import { useState, useRef, useEffect } from "react";
 
 const toX = (v) => (typeof v === "number" ? { x: v } : v);
@@ -9,6 +9,7 @@ const styles = [0, 0].map(toX);
 
 export function InitialStyleAnimate() {
   const [index, setIndex] = useState(0);
+  const [mode, cycleMode] = useCycle("boring", "smiling");
   const prevValues = useRef({});
   useEffect(() => {
     prevValues.current = {
@@ -19,7 +20,11 @@ export function InitialStyleAnimate() {
   }, [index]);
   const stringifyEqual = (v1, v2) => JSON.stringify(v1) === JSON.stringify(v2);
   return (
-    <motion.div>
+    <motion.div
+      initial={false}
+      animate={mode}
+      variants={{ smiling: { transition: { staggerChildren: 0.1 } } }}
+    >
       <motion.div
         style={{
           position: "relative",
@@ -33,7 +38,10 @@ export function InitialStyleAnimate() {
         transition={{ ease: "easeInOut" }}
         onClick={() => setIndex((i) => (i < initials.length - 1 ? i + 1 : 0))}
       >
-        <motion.div style={{ position: "absolute", top: 0, left: 0 }}>
+        <motion.div
+          style={{ position: "absolute", top: 0, left: 0 }}
+          variants={{ boring: { y: `-60vh` }, smiling: { y: 0 } }}
+        >
           🤨
         </motion.div>
         <motion.div
@@ -52,6 +60,10 @@ export function InitialStyleAnimate() {
             display: "grid",
             gridTemplateColumns: "auto 1fr",
             gridColumnGap: 8,
+          }}
+          variants={{
+            boring: { rotate: 0, y: 120 },
+            smiling: { rotate: -5, y: 0 },
           }}
         >
           <div>initial</div>
@@ -91,7 +103,13 @@ export function InitialStyleAnimate() {
             {JSON.stringify(styles[index])}
           </div>
         </motion.div>
-        <motion.div style={{ position: "absolute", top: 20, left: -100 }}>
+        <motion.div
+          style={{ position: "absolute", top: 20, left: -100 }}
+          variants={{
+            boring: { x: `-100vw` },
+            smiling: { x: 0, transition: { ease: "easeOut" } },
+          }}
+        >
           💪
         </motion.div>
         <motion.div
@@ -102,13 +120,29 @@ export function InitialStyleAnimate() {
             scaleX: -1,
             rotate: 30,
           }}
+          variants={{
+            boring: { x: `100vw` },
+            smiling: { x: 0, transition: { ease: "easeOut" } },
+          }}
         >
           💪
         </motion.div>
-        <motion.div style={{ position: "absolute", top: 215, left: 40 }}>
+        <motion.div
+          style={{ position: "absolute", top: 215, left: 40 }}
+          variants={{
+            boring: { y: `100vh` },
+            smiling: { y: 0, transition: { ease: "easeOut" } },
+          }}
+        >
           🦵
         </motion.div>
-        <motion.div style={{ position: "absolute", top: 225, left: 10 }}>
+        <motion.div
+          style={{ position: "absolute", top: 225, left: 10 }}
+          variants={{
+            boring: { x: `100vw` },
+            smiling: { x: 0, transition: { ease: "easeOut" } },
+          }}
+        >
           🦵
         </motion.div>
       </motion.div>
@@ -125,7 +159,9 @@ export function InitialStyleAnimate() {
           padding: 8,
         }}
       >
-        <div style={{ position: "absolute", left: 0 }}>0</div>
+        <div style={{ position: "absolute", left: 0 }} onClick={cycleMode}>
+          0
+        </div>
         <div style={{ position: "absolute", left: 200 }}>200</div>
         <div style={{ position: "absolute", left: 400 }}>400</div>
       </div>
