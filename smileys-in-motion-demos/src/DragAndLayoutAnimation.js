d59329dcd9350923177706aa6e008a9d0c117c4a
Linton Ye <linton@jimulabs.com>
2020-11-10 17:11:01 -0800

    Turn into single component impl -> not working

--- smileys-in-motion-demos/src/DragAndLayoutAnimation.js
+++ smileys-in-motion-demos/src/DragAndLayoutAnimation.js
@@ -13,78 +13,67 @@ import move from "array-move";
 
 /* eslint-disable jsx-a11y/accessible-emoji */
 
-function Item({ onMove, index, title }) {
-  const [dragging, setDragging] = useState(false);
-  return (
-    <motion.li
-      style={{
-        cursor: "pointer",
-        background: "#444",
-        borderRadius: 8,
-        minWidth: 200,
-        marginTop: 8,
-        padding: 8,
-        position: "relative", // need this to enable z-index
-      }}
-      whileHover={{ scale: 1.1, zIndex: 1 }}
-      whileTap={{
-        scale: 1.2,
-        zIndex: 2,
-        boxShadow: "1px 1px 15px rgb(0,0,0,0.25)",
-      }}
-      drag="y"
-      layout
-      onViewportBoxUpdate={(box, delta) => {
-        if (dragging) {
-          const height = box.y.max - box.y.min;
-          const dragThreshold = 0.3;
-          // Assumption: all items are of the same height.
-          const targetIndex =
-            (delta.y.translate > 0
-              ? Math.floor(delta.y.translate / height + dragThreshold)
-              : Math.ceil(delta.y.translate / height - dragThreshold)) + index;
-          if (targetIndex !== index) {
-            // console.log(
-            //   { detaY: delta.y.translate },
-            //   { targetIndex },
-            //   { index },
-            //   { title }
-            // );
-            onMove(index, targetIndex);
-          }
-        }
-      }}
-      onDragStart={() => {
-        setDragging(true);
-      }}
-      onDragEnd={() => {
-        setDragging(false);
-      }}
-    >
-      {title}
-    </motion.li>
-  );
-}
-
 const allFaces = "😇 😃 🥱 🤩 😢 😎 🤯"
   .split(" ")
   .map((f, idx) => `${idx} ${f}`);
 
 function DragToReorder() {
   const [faces, setFaces] = useState(allFaces);
+  const [draggingIndex, setDraggingIndex] = useState(-1);
   return (
     <ul style={{ listStyleType: "none", fontSize: 50 }}>
       {faces.map((f, index) => (
-        <Item
+        <motion.li
+          style={{
+            cursor: "pointer",
+            background: "#444",
+            borderRadius: 8,
+            minWidth: 200,
+            marginTop: 8,
+            padding: 8,
+            position: "relative", // need this to enable z-index
+          }}
           key={f}
-          index={index}
-          title={f}
-          onMove={(idx, targetIndex) => {
-            if (0 <= targetIndex && targetIndex < faces.length) {
-              setFaces((fs) => move(fs, idx, targetIndex));
+          whileHover={{ scale: 1.1, zIndex: 1 }}
+          whileTap={{
+            scale: 1.2,
+            zIndex: 2,
+            boxShadow: "1px 1px 15px rgb(0,0,0,0.25)",
+          }}
+          drag="y"
+          layout
+          onViewportBoxUpdate={(box, delta) => {
+            if (draggingIndex) {
+              const height = box.y.max - box.y.min;
+              const dragThreshold = 0.3;
+              // Assumption: all items are of the same height.
+              const targetIndex =
+                (delta.y.translate > 0
+                  ? Math.floor(delta.y.translate / height + dragThreshold)
+                  : Math.ceil(delta.y.translate / height - dragThreshold)) +
+                index;
+              if (targetIndex !== index) {
+                console.log(
+                  { detaY: delta.y.translate },
+                  { targetIndex },
+                  { index }
+                  // { title }
+                );
+                if (0 <= targetIndex && targetIndex < faces.length) {
+                  setFaces((fs) => move(fs, index, targetIndex));
+                }
+              }
             }
           }}
-        />
+          onDragStart={() => {
+            setDraggingIndex(index);
+          }}
+          onDragEnd={() => {
+            setDraggingIndex(-1);
+          }}
+        >
+          {f}
+        </motion.li>
       ))}
     </ul>
   );
