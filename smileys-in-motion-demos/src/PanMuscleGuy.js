b96967752eb94e8cbd6ab0b3e6bc0ddc6d6be7a6
Linton Ye <linton@jimulabs.com>
2020-11-11 23:05:06 -0800

    Use ref to get boundingbox

--- smileys-in-motion-demos/src/PanMuscleGuy.js
+++ smileys-in-motion-demos/src/PanMuscleGuy.js
@@ -1,28 +1,42 @@
 import * as React from "react";
 import { motion, useMotionValue, useTransform } from "framer-motion";
 
+function useInitialViewportBBox() {
+  const ref = React.useRef();
+  const [bbox, setBbox] = React.useState(0);
+  React.useEffect(() => {
+    setBbox(ref.current.getBoundingClientRect());
+  }, [ref]);
+  return [bbox, ref];
+}
+
+const clamp = (v, min, max) => Math.min(max, Math.max(v, min));
+
 export function PanMuscleGuy() {
   const rotateLeft = useMotionValue(0);
   const rotateRight = useTransform(rotateLeft, (r) => -r);
-  function getAngleToBottomRight(e, info) {
-    const { bottom, right } = e.target.getBoundingClientRect();
+  const [leftArmBoundingBox, leftArmRef] = useInitialViewportBBox();
+  function getAngleToBottomRight(info) {
+    const { bottom, right } = leftArmBoundingBox;
     const x = right - info.point.x;
     const y = bottom - info.point.y;
     const a = (Math.atan2(y, x) * 180) / Math.PI;
     return a;
   }
-  const clamp = (v, min, max) => Math.min(max, Math.max(v, min));
-  let initialAngle = 0;
+  let initialPointerAngle = 0,
+    initialRotateLeft = 0;
   return (
     <div style={{ display: "flex" }}>
       <motion.div
         style={{ rotate: rotateLeft, originX: 1, cursor: "pointer" }}
+        ref={leftArmRef}
         onPanStart={(e, info) => {
-          initialAngle = getAngleToBottomRight(e, info);
+          initialPointerAngle = getAngleToBottomRight(info);
+          initialRotateLeft = rotateLeft.get();
         }}
         onPan={(e, info) => {
-          const delta = getAngleToBottomRight(e, info) - initialAngle;
-          const newRotate = clamp(rotateLeft.get() + delta, -30, 30);
+          const delta = getAngleToBottomRight(info) - initialPointerAngle;
+          const newRotate = clamp(initialRotateLeft + delta, -30, 30);
           rotateLeft.set(newRotate);
         }}
       >
