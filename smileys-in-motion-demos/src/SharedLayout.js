0f7a8eed1f30c0d617c2dd3ef63aae63bc2f5d0b
Linton Ye <linton@jimulabs.com>
2020-11-13 23:18:20 -0800

    Add tabs

--- smileys-in-motion-demos/src/SharedLayout.js
+++ smileys-in-motion-demos/src/SharedLayout.js
@@ -2,6 +2,8 @@ import * as React from "react";
 import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
+/* eslint-disable jsx-a11y/accessible-emoji */
+
 function SportModal({ type, onClose }) {
   const toolbar = (
     <motion.div
@@ -106,7 +108,7 @@ function DialogTransition() {
             gridGap: 8,
           }}
         >
-          {sports.map((a) => (
+          {sports.map((s) => (
             <motion.div
               style={{
                 padding: 16,
@@ -116,11 +118,11 @@ function DialogTransition() {
                 boxShadow: "0 2px 8px rgba(0,0,0,0.4)",
               }}
               whileHover={{ scale: 1.1 }}
-              onTap={() => setSelectedSport(a)}
-              layoutId={`card-${a}`}
-              key={a}
+              onTap={() => setSelectedSport(s)}
+              layoutId={`card-${s}`}
+              key={s}
             >
-              <motion.div layoutId={`emoji-${a}`}>{a}</motion.div>
+              <motion.div layoutId={`emoji-${s}`}>{s}</motion.div>
             </motion.div>
           ))}
         </motion.div>
@@ -137,10 +139,96 @@ function DialogTransition() {
   );
 }
 
+function Tab({ title, color, active, onSelect }) {
+  return (
+    <motion.div onClick={onSelect} style={{ cursor: "default" }}>
+      <motion.div
+        whileHover={{ color: "#fff" }}
+        animate={{ color: active ? "#fff" : "#999" }}
+        style={{ padding: "8px 16px 8px 16px" }}
+      >
+        {title}
+      </motion.div>
+      {active && (
+        <motion.div
+          layoutId="indicator"
+          style={{ height: 4, width: "100%", borderRadius: 4 }}
+          animate={{ background: color }}
+        />
+      )}
+    </motion.div>
+  );
+}
+
+function Tabs() {
+  const [index, setIndex] = useState(0);
+  const tabs = [
+    { title: "Apple", color: "#F22" },
+    { title: "Plum", color: "#6565fe" },
+    { title: "Banana", color: "#2F2" },
+  ];
+  return (
+    <AnimateSharedLayout>
+      <div style={{ display: "flex", fontSize: 25, color: "#eee" }}>
+        {tabs.map((tab, i) => (
+          <Tab
+            {...tab}
+            key={tab.title}
+            onSelect={() => setIndex(i)}
+            active={index === i}
+          />
+        ))}
+      </div>
+    </AnimateSharedLayout>
+  );
+}
+
+function LayoutSwitcher({ children, layouts = undefined }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const finalLayouts = layouts || React.Children.toArray(children);
+  return (
+    <AnimateSharedLayout>
+      <div
+        onClick={() =>
+          setCurrentIndex((i) => (i < finalLayouts.length - 1 ? i + 1 : 0))
+        }
+      >
+        {finalLayouts[currentIndex]}
+      </div>
+    </AnimateSharedLayout>
+  );
+}
+
+function DOMReuse() {
+  return (
+    <LayoutSwitcher
+      layouts={[
+        <motion.div>
+          <motion.div layoutId="happy">😄</motion.div>
+          <motion.div layoutId="sad">😡</motion.div>
+        </motion.div>,
+        <motion.div>
+          <motion.div layoutId="happy">😄</motion.div>
+        </motion.div>,
+        <motion.div>
+          <motion.div layoutId="sad">😡</motion.div>
+          <motion.div layoutId="happy">😄</motion.div>
+        </motion.div>,
+      ]}
+    ></LayoutSwitcher>
+  );
+}
+
+function AspectRatio() {
+  return;
+}
+
 export function SharedLayout() {
   return (
     <>
-      <DialogTransition />
+      {/* <DOMReuse /> */}
+      <Tabs />
+      {/* <DialogTransition /> */}
     </>
   );
 }
