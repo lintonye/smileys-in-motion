0acfd7081f1cb985ef554f881993262c61461c92
Linton Ye <linton@jimulabs.com>
2020-11-16 17:52:13 -0800

    Remove solution

--- smileys-in-motion-demos/src/ImperativeControls.js
+++ smileys-in-motion-demos/src/ImperativeControls.js
@@ -5,34 +5,7 @@ import { useRef } from "react";
 /* eslint-disable jsx-a11y/accessible-emoji */
 
 function AnimationControls() {
-  const controls = useAnimation();
-  async function dance() {
-    await controls.start({ x: 200 });
-    await controls.start({ x: -200 });
-    await controls.start({ scaleX: -1 });
-    await controls.start({ x: 0 });
-    await controls.start({ scaleX: 1 });
-  }
-  async function jump() {
-    await controls.start({
-      y: -200,
-      x: -100,
-      transition: { ease: "easeInOut" },
-    });
-    await controls.start({ y: 0, x: -100 });
-  }
-  return (
-    <motion.div
-      animate={controls}
-      style={{ fontSize: 200 }}
-      onTap={async () => {
-        await jump();
-        await dance();
-      }}
-    >
-      🕺
-    </motion.div>
-  );
+  return <motion.div style={{ fontSize: 200 }}>🕺</motion.div>;
 }
 
 export function ImperativeControls() {
