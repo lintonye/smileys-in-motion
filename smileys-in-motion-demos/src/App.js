b1478d7083808674e9639c27dfeae3b2126204b8
Linton Ye <linton@jimulabs.com>
2020-11-17 18:01:55 -0800

    Initialize files

--- smileys-in-motion-demos/src/App.js
+++ smileys-in-motion-demos/src/App.js
@@ -1,41 +1,7 @@
 import * as React from "react";
-import { Fistful } from "./Fistful";
-import { SVGSmiley } from "./SVGSmiley";
-import { ThemeToggle } from "./ThemeToggle";
-import { EmojiPicker } from "./EmojiPicker";
-import { LayoutExamples } from "./LayoutExamples";
-import { MountUnmount } from "./MountUnmount";
-import { InitialStyleAnimate } from "./InitialStyleAnimate";
-import { DragAndLayoutAnimation } from "./DragAndLayoutAnimation";
-import { PanMuscleGuy } from "./PanMuscleGuy";
-import { SharedLayout } from "./SharedLayout";
-import { SharedElementWithRouter } from "./SharedElementWithRouter";
-import { CircularSliderDemo } from "./CircularSliderDemo";
+
+import { LearnReactDesignMotionPage } from "./LearnReactDesignMotionPage";
 
 export default function App() {
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: 120,
-        height: "100vh",
-        userSelect: "none",
-      }}
-    >
-      {/* <Fistful /> */}
-      {/* <SVGSmiley /> */}
-      {/* <ThemeToggle /> */}
-      {/* <EmojiPicker /> */}
-      {/* <LayoutExamples /> */}
-      {/* <MountUnmount /> */}
-      {/* <InitialStyleAnimate /> */}
-      {/* <DragAndLayoutAnimation /> */}
-      {/* <PanMuscleGuy /> */}
-      {/* <SharedLayout /> */}
-      {/* <SharedElementWithRouter /> */}
-      <CircularSliderDemo />
-    </div>
-  );
+  return <LearnReactDesignMotionPage />;
 }
---
+++ smileys-in-motion-demos/src/LearnReactDesignMotionPage/index.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { motion, useTransform, useViewportScroll } from "framer-motion";
+
+function Welcome() {
+  return (
+    <motion.div>
+      <motion.div>🤨</motion.div>
+    </motion.div>
+  );
+}
+
+export function LearnReactDesignMotionPage() {
+  return <motion.div>Page</motion.div>;
+}
