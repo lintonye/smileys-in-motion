6e6ee680fabadcf97dde035800ca7d48794da19f
Linton Ye <linton@jimulabs.com>
2020-11-25 15:22:48 -0800

    Remove MS font as mono

--- smileysinmotion.com/tailwind.config.js
+++ smileysinmotion.com/tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       // },
       fontFamily: {
         sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+        mono: ["Courier New", "Courier", "monospace"],
       },
     },
   },
