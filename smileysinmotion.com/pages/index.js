f4888a2151cad86988a7c56436b429f11b9981a8
Linton Ye <linton@jimulabs.com>
2020-11-27 09:41:22 -0800

    Add social card

--- smileysinmotion.com/pages/index.js
+++ smileysinmotion.com/pages/index.js
@@ -2,14 +2,29 @@ import Head from "next/head";
 import { Home } from "../components/Home";
 
 export default function HomePage() {
+  const siteName = "Smiley In Motion";
+  const pageTitle = `Get really good at Framer Motion | ${siteName}`;
+  const description =
+    "A comprehensive Framer Motion course on mental models, tips &amp; tricks, and common pitfalls";
+  const twitterHandle = "lintonye";
+  const previewImage = "/images/social-card.png";
   return (
     <>
       <Head>
-        <title>Get really good at Framer Motion | Smiley In Motion</title>
-        <meta
-          name="description"
-          content="A comprehensive Framer Motion course on mental models, tips &amp; tricks, and common pitfalls"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <title>{pageTitle}</title>
+        <meta name="description" content={description} />
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" key="twcard" />
+        <meta name="twitter:creator" content={twitterHandle} key="twhandle" />
+
+        {/* Open Graph */}
+        <meta property="og:url" content={currentURL} key="ogurl" />
+        <meta property="og:image" content={previewImage} key="ogimage" />
+        <meta property="og:site_name" content={siteName} key="ogsitename" />
+        <meta property="og:title" content={pageTitle} key="ogtitle" />
+        <meta property="og:description" content={description} key="ogdesc" />
       </Head>
       <Home />
     </>
