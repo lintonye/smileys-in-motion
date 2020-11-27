8dda9e2f12ae8cc29b65745184eba3e3b9eaa731
Linton Ye <linton@jimulabs.com>
2020-11-26 18:02:13 -0800

    Add title and meta

--- smileysinmotion.com/pages/index.js
+++ smileysinmotion.com/pages/index.js
@@ -1,4 +1,17 @@
 import Head from "next/head";
 import { Home } from "../components/Home";
 
-export default Home;
+export default function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>Get really good at Framer Motion</title>
+        <meta
+          name="description"
+          content="A comprehensive Framer Motion course on mental models, tips &amp; tricks, and common pitfalls"
+        />
+      </Head>
+      <Home />
+    </>
+  );
+}
